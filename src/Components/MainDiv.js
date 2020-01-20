ac4f61fb1d31ac5c8bec47455575cbe9433974e3
Nikhil Rana <nrana@quark.com>
2020-01-20 18:44:00 +0530

    Second Commit

--- src/Components/MainDiv.js
+++ src/Components/MainDiv.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 
 class MainDiv extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            flag : false
+        }
+        this.sum  = 0;
+        this.counter = 0;
+    }
+
+    sumFun = (val) => {
+        this.sum += val;
+        console.log(val);
+        this.counter++;
+        if(this.counter === 4) {
+            this.counter = 0;
+            if(this.sum === 14) {
+                console.log("done");
+            }
+        }
+    } 
+
     render() {
         return(
             <div className="mainContainer">
-                <button>14</button>
-                <button>4</button>
-                <button>4</button>
-                <button>5</button>
-                <button>2</button>
-                <button>3</button>
+                <button type="submit">14</button>
+                <button type="submit" onClick={this.sumFun}>4</button>
+                <button type="submit" onClick={this.sumFun}>4</button>
+                <button type="submit" onClick={this.sumFun}>5</button>
+                <button type="submit" onClick={this.sumFun}>2</button>
+                <button type="submit" onClick={this.sumFun}>3</button>
+                <button type="submit" onClick={this.sumFun}>4</button>
             </div>
         );
     }
