a0bd860407236cb20120df5331952ce47df8c20d
Nikhil Rana <nrana@quark.com>
2020-01-20 18:55:32 +0530

    second commit

--- src/Components/MainDiv.js
+++ src/Components/MainDiv.js
@@ -12,14 +12,19 @@ class MainDiv extends Component {
     }
 
     sumFun = (val) => {
-        this.sum += val;
-        console.log(val);
+        this.sum += parseInt(val.target.value);
+        //console.log(this.sum);
+        //console.log(val.target.value);
         this.counter++;
         if(this.counter === 4) {
             this.counter = 0;
             if(this.sum === 14) {
                 console.log("done");
             }
+            else {
+                console.log("Wrong Answer");
+            }
+            this.sum = 0;
         }
     } 
 
@@ -27,12 +32,12 @@ class MainDiv extends Component {
         return(
             <div className="mainContainer">
                 <button type="submit">14</button>
-                <button type="submit" onClick={this.sumFun}>4</button>
-                <button type="submit" onClick={this.sumFun}>4</button>
-                <button type="submit" onClick={this.sumFun}>5</button>
-                <button type="submit" onClick={this.sumFun}>2</button>
-                <button type="submit" onClick={this.sumFun}>3</button>
-                <button type="submit" onClick={this.sumFun}>4</button>
+                <button type="submit" value="4" onClick={this.sumFun}>4</button>
+                <button type="submit" value="4"onClick={this.sumFun}>4</button>
+                <button type="submit" value="5" onClick={this.sumFun}>5</button>
+                <button type="submit" value="2" onClick={this.sumFun}>2</button>
+                <button type="submit" value="3" onClick={this.sumFun}>3</button>
+                <button type="submit" value="4" onClick={this.sumFun}>4</button>
             </div>
         );
     }
